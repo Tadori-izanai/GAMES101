fafd0ca5fe267f7a0cc9058e6e280c40a43af749
lxl <u17814@outlook.com>
2023-01-26 18:57:35 +0800

    hw0 and hw1 done

---
+++ hw0/main.cpp
@@ -0,0 +1,67 @@
+#include<cmath>
+#include<eigen3/Eigen/Core>
+#include<eigen3/Eigen/Dense>
+#include<iostream>
+
+#define IGNORE
+
+int main() {
+#ifndef IGNORE
+    // Basic Example of cpp
+    std::cout << "Example of cpp \n";
+    float a = 1.0, b = 2.0;
+    std::cout << a << std::endl;
+    std::cout << a/b << std::endl;
+    std::cout << std::sqrt(b) << std::endl;
+    std::cout << std::acos(-1) << std::endl;
+    std::cout << std::sin(30.0/180.0*acos(-1)) << std::endl;    // pi = acos(-1)
+
+    // Example of vector
+    std::cout << "Example of vector \n";
+    // vector definition
+    Eigen::Vector3f v(1.0f,2.0f,3.0f);
+    Eigen::Vector3f w(1.0f,0.0f,0.0f);
+    // vector output
+    std::cout << "Example of output \n";
+    std::cout << v << std::endl;
+    // vector add
+    std::cout << "Example of add \n";
+    std::cout << v + w << std::endl;
+    // vector scalar multiply
+    std::cout << "Example of scalar multiply \n";
+    std::cout << v * 3.0f << std::endl;
+    std::cout << 2.0f * v << std::endl;
+
+    // Example of matrix
+    std::cout << "Example of matrix \n";
+    // matrix definition
+    Eigen::Matrix3f i,j;
+    i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
+    j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
+    // matrix output
+    std::cout << "Example of output \n";
+    std::cout << i << std::endl;
+
+
+    // matrix add i + j
+    std::cout << "\ni + j =\n" << i + j << std::endl;
+    // matrix scalar multiply i * 2.0
+    std::cout << "\ni * 2.0 =\n" << i * 2.0f << std::endl;
+    // matrix multiply i * j
+    std::cout << "\ni * j =\n" << i * j << std::endl;
+    // matrix multiply vector i * v
+    std::cout << "\ni * v =\n" << i * v << std::endl;
+#endif
+
+    const double PI = std::acos(-1);
+    Eigen::Vector3d p(2, 1, 1);
+
+    double theta = PI / 4;
+    Eigen::Matrix3d rotationAndTranslation;
+    rotationAndTranslation << std::cos(theta), -std::sin(theta), 1,
+                              std::sin(theta), std::sin(theta), 2,
+                              0, 0, 1;
+    std::cout << rotationAndTranslation * p << std::endl;
+
+    return 0;
+}
--- hw1/main.cpp
+++ hw1/main.cpp
@@ -4,7 +4,15 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
+//#define EXTRA_PART
+
 constexpr double MY_PI = 3.1415926;
+
+#ifdef EXTRA_PART
+const Eigen::Vector3f X_AXIS(1, 0, 0);
+const Eigen::Vector3f Y_AXIS(0, 1, 0);
+const Eigen::Vector3f Z_AXIS(0, 0, 1);
+#endif
 
 Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
 {
@@ -22,30 +30,83 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
 {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
 
-    // TODO: Implement this function
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
+    float rotation_angle_rad = (float)MY_PI / 180 * rotation_angle;
+    model << std::cos(rotation_angle_rad), -std::sin(rotation_angle_rad), 0, 0,
+             std::sin(rotation_angle_rad), std::cos(rotation_angle_rad), 0, 0,
+             0, 0, 1, 0,
+             0, 0, 0, 1;
 
     return model;
 }
 
-Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
-                                      float zNear, float zFar)
+Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio, float zNear, float zFar)
 {
     // Students will implement this function
 
     Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
 
-    // TODO: Implement this function
     // Create the projection matrix for the given parameters.
     // Then return it.
+
+    /* perspective to orthographic */
+    Eigen::Matrix4f perspective_to_orthographic;
+    perspective_to_orthographic <<
+        zNear, 0, 0, 0,
+        0, zNear, 0, 0,
+        0, 0, (zNear + zFar), -zNear * zFar,
+        0, 0, 1, 0;
+
+    /* scale */
+    float eye_fov_rad = (float)MY_PI / 180 * eye_fov;
+    float t = -zNear * std::tan(eye_fov_rad / 2);
+    Eigen::Matrix4f scale = Eigen::Vector4f(
+        1.0f / (t * aspect_ratio), 1.0f / t, 2.0f / (zNear - zFar), 1.0f
+    ).asDiagonal();
+
+    /* translation */
+    Eigen::Matrix4f translation = Eigen::Matrix4f::Identity();
+    translation(2, 3) = -(zNear + zFar) / 2;
+
+    Eigen::Matrix4f orthographic = scale * translation;
+    projection = orthographic * perspective_to_orthographic * projection;
 
     return projection;
 }
 
+#ifdef EXTRA_PART
+Eigen::Matrix4f get_rotation(const Eigen::Vector3f &axis, float angle) {
+    float angle_rad = (float)MY_PI / 180 * angle;
+
+    Eigen::Vector3f axis_normalized = axis / axis.norm();
+    float x = axis_normalized.x();
+    float y = axis_normalized.y();
+    float z = axis_normalized.z();
+
+    Eigen::Matrix3f outer;
+    outer << 0, -z, y,
+            z, 0, -x,
+            -y, x, 0;
+
+    Eigen::Matrix3f rotation = std::cos(angle_rad) * Eigen::Matrix3f::Identity()
+                                + (1.0 - std::cos(angle_rad)) * axis_normalized * axis_normalized.transpose()
+                                + std::sin(angle_rad) * outer;
+
+    Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
+    model.block<3, 3>(0, 0) = rotation;
+    return model;
+}
+#endif
+
 int main(int argc, const char** argv)
 {
     float angle = 0;
+
+#ifdef EXTRA_PART
+    Eigen::Vector3f axis = Z_AXIS;
+#endif
+
     bool command_line = false;
     std::string filename = "output.png";
 
@@ -90,7 +151,11 @@ int main(int argc, const char** argv)
     while (key != 27) {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
+#ifndef EXTRA_PART
         r.set_model(get_model_matrix(angle));
+#else
+        r.set_model(get_rotation(axis, angle));
+#endif
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
@@ -109,7 +174,18 @@ int main(int argc, const char** argv)
         else if (key == 'd') {
             angle -= 10;
         }
+#ifdef EXTRA_PART
+        else if (key == 'x') {
+            angle = 0;
+            axis = X_AXIS;
+        } else if (key == 'y') {
+            angle = 0;
+            axis = Y_AXIS;
+        } else if (key == 'z') {
+            angle = 0;
+            axis = Z_AXIS;
+        }
+#endif
     }
-
     return 0;
 }
